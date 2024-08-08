72751c291dc1a0d9f4885f8804c594e12d9d0708
David Roa <david_roa_06@hotmail.com>
2024-08-07 20:10:00 -0500

    Add forEach function

--- holamundo.js
+++ holamundo.js
@@ -1,2 +1,18 @@
 let nombre = 'David Roa'
 console.log('Hola mundo', nombre);
+
+const pi = 3.1416;
+let boolean= true;
+
+if (boolean){
+    console.log('la variable es verdadera');
+} else{
+    console.log('la varible es falsa');
+}
+
+const arreglos =['coco','Piña','Manzana','Banana'];
+
+arreglos.forEach((fruta)=>{
+    console.log('El nombre de la fruta es:',fruta);
+
+})
