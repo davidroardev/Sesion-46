4d6c6b2c6ea326bbbbc51d8abd9f1ad540b9a583
David Roa <david_roa_06@hotmail.com>
2024-08-07 20:43:35 -0500

    Ejemplo de typescript

---
+++ holamundo.ts
@@ -0,0 +1,21 @@
+let booleanTrue: boolean = true; 
+let booleanFalse:boolean = false;
+
+const numeropi: number = 3.1416;
+
+interface User{
+    name: string;
+    age: number;
+}
+
+const persona:User = {
+    name: 'David Roa',
+    age: 22
+}
+
+const suma =(numero1:number,numero2:number)=>{
+    console.log(numero1+numero2);
+
+};
+
+suma(2.822222222222,25)
